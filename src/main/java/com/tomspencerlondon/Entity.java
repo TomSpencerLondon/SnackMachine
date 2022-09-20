c74e04eda3a5c466b03a5cbfaf71e9d2c353042a
Tom Spencer <thomas.spencer@codurance.com>
2022-09-20 23:29:20 +0100

    Add Entity abstract class

---
+++ src/main/java/com/tomspencerlondon/Entity.java
@@ -0,0 +1,24 @@
+package com.tomspencerlondon;
+
+public abstract class Entity {
+  long id;
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+
+    Entity entity = (Entity) o;
+
+    return id == entity.id;
+  }
+
+  @Override
+  public int hashCode() {
+    return (int) (id ^ (id >>> 32));
+  }
+}
