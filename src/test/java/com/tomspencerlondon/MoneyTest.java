002dd127adb40f184a972995c0394469661be772
Tom Spencer <thomas.spencer@codurance.com>
2022-09-21 00:00:52 +0100

    Add test for not equal amounts

--- src/test/java/com/tomspencerlondon/MoneyTest.java
+++ src/test/java/com/tomspencerlondon/MoneyTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 public class MoneyTest {
 
   @Test
-  void sum_of_tow_moneys_produces_correct_result() {
+  void sum_of_two_moneys_produces_correct_result() {
     Money money1 = new Money(1, 2, 3, 4, 5, 6, 7);
     Money money2 = new Money(1, 2, 3, 4, 5, 6, 7);
 
@@ -16,5 +16,16 @@ public class MoneyTest {
 
     assertThat(result)
         .isEqualTo(new Money(2, 4, 6, 8, 10, 12, 14));
+  }
+
+  @Test
+  void two_money_instances_do_not_equal_if_contain_different_money_amounts() {
+    Money dollar = new Money(0, 0, 0, 1, 0, 0, 0);
+    Money hundredCents = new Money(100, 0, 0, 0, 0, 0, 0);
+
+    assertThat(dollar)
+        .isNotEqualTo(hundredCents);
+    assertThat(dollar.hashCode())
+        .isNotEqualTo(hundredCents.hashCode());
   }
 }
